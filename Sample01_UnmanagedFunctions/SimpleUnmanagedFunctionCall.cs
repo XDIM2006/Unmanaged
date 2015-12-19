0f4e5b6f34bebe85ae09db0def96e349cee31133
xdim <xdim2006@yandex.ru>
2015-12-20 00:04:10 +0300

    unmanaged

---
+++ Sample01_UnmanagedFunctions/SimpleUnmanagedFunctionCall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskUnmanaged;
+
+namespace Sample01_UnmanagedFunctions
+{
+	[TestClass]
+	public class SimpleUnmanagedFunctionCall
+	{
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var liba = (IPowrProfLibaCall)new PowrProfLibaCall();
+
+            Console.WriteLine(
+                liba.LastSleepTime());
+
+            Console.WriteLine(
+                liba.LastWakeTime());
+            Console.WriteLine(
+                liba.SystemReserveHiberFile(true));
+            Console.WriteLine(
+                liba.SystemReserveHiberFile(false));
+
+            Console.WriteLine(liba.SystemBatteryState());
+
+            Console.WriteLine(liba.SystemPowerInfo());
+
+        }
+	}
+}
